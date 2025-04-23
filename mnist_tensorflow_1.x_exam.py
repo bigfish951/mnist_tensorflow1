7ae39cd8549957744d12c9c534281fcf42725816
bigfish951 <bigfish951013@gmail.com>
2025-04-23 17:35:37 +0900

    Update mnist_tensorflow_1.x_exam.py

--- mnist_tensorflow_1.x_exam.py
+++ mnist_tensorflow_1.x_exam.py
@@ -1,7 +1,7 @@
-import tensorflow.compat.v1 as tf
+import tensorflow.compat.v1 as tf 
 tf.disable_v2_behavior() 
 import keras
-import pandas as pd
+import pandas as pd (정상수행을 위해 수정된 부분)
 
 
 # Model Parameters
